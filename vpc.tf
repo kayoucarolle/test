32da62e270f855bb39fd3df5128313b1c992c663
kayoucarolle <123230091+kayoucarolle@users.noreply.github.com>
2023-08-19 12:50:59 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,11 @@
 provider "aws" {
     region = "us-east-1"
 }
+
 resource "aws_vpc" "vpcproducts" {
 
   cidr_block = "192.168.0.0/16"
-
-
- 
- tags = {
+  tags = {
     Name = "vpc-fruits"
     }
   
